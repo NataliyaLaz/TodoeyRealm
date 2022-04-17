89e5f587b92dd7c994a091c90ffa899087ccb693
NataliyaLaz <nataliya.laz@gmail.com>
2022-04-17 09:50:31 +0300

    realm ready

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
 @UIApplicationMain
@@ -17,49 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
         
-//        do {
-//            let realm = try Realm()
-//            }
-//        } catch {
-//            print("Error initializing new realm, \(error)")
-//        }
+        //print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            _ = try Realm()
+        } catch {
+            print("Error initializing new realm, \(error)")
+        }
         return true
-   }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-       // self.saveContext()
+        
     }
-    
-    
-    // MARK: - Core Data stack
-    
-//    lazy var persistentContainer: NSPersistentContainer = {
-//
-//        let container = NSPersistentContainer(name: "DataModel")
-//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        })
-//        return container
-//    }()
-    
-    // MARK: - Core Data Saving support
-    
-//    func saveContext () {
-//        let context = persistentContainer.viewContext
-//        if context.hasChanges {
-//            do {
-//                try context.save()
-//            } catch {
-//                let nserror = error as NSError
-//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//            }
-//        }
-//    }
 }
 
--- Todoey/Data Model/item.swift
+++ Todoey/Data Model/item.swift
@@ -10,8 +10,8 @@ import Foundation
 import RealmSwift
 
 class Item: Object{
-   @objc dynamic var title: String = ""
-   @objc dynamic var done: Bool = false
-   @objc dynamic var dateCreated: Date?
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
